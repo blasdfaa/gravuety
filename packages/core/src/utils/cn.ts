ffc4cf0efcb9dcada958a0a36f7ecf6a8bbf939e
Viktor Zdorovtsev <v.zdvacew@gmail.com>
2025-04-09 21:43:36 +0700

    chore: use local classname fn

--- packages/core/src/utils/cn.ts
+++ packages/core/src/utils/cn.ts
@@ -1,5 +1,5 @@
-import { withNaming } from '@bem-react/classname'
 import { NAMESPACE } from '../constants'
+import { withNaming } from './classname'
 
 export const cn = withNaming({ e: '__', m: '_' })
 export const block = withNaming({ n: NAMESPACE, e: '__', m: '_' })
